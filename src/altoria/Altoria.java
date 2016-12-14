40158dfd83717675a411780171d28004feda2fba
4binf9 <4binf9@PC7318.IISGASTALDI.local>
2016-12-14 15:32:00 +0100

    Inizializzazione

---
+++ src/altoria/Altoria.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package altoria;
+
+/**
+ *
+ * @author 4binf9
+ */
+public class Altoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int lol;
+    }
+            
+}
