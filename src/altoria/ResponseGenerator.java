d4b242370cda98a0630beea1d331d48e31b9198c
4binf9 <4binf9@PC7318.IISGASTALDI.local>
2016-12-21 15:10:59 +0100

    21/12/2016

---
+++ src/altoria/ResponseGenerator.java
@@ -0,0 +1,10 @@
+package altoria;
+
+//Analyze user input and generate an appropriate response
+public class ResponseGenerator {
+    private ArrayList<String> key
+    
+    public static void main(String[] argsS){
+        
+    }
+}
