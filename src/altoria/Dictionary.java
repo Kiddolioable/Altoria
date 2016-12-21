4396b59428b84e1cb7ea50803cc0f0542885172b
4binf9 <4binf9@PC7318.IISGASTALDI.local>
2016-12-21 14:13:52 +0100

    21/12/2016

--- src/altoria/Dictionary.java
+++ src/altoria/Dictionary.java
@@ -1,5 +1,6 @@
 package altoria;
 
 public class Dictionary {
+    Quindi che metodo utilizziamo per analisi del testo? Dobbiamo pensare a un modo per far sì che il bot capisca cosa dice the utent.
     
 }
