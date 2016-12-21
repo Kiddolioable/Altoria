feab37e6337a0a556b7d3b809e6e1ee9634c38aa
4binf9 <4binf9@PC7318.IISGASTALDI.local>
2016-12-21 14:29:29 +0100

    21/12/2016

--- src/altoria/Altoria.java
+++ src/altoria/Altoria.java
@@ -4,7 +4,9 @@ public class Altoria
 {
     public static void main(String[] args) 
     {
-        
+        System.out.println("/-------------------------------\\"
+                       + "\n| A L T O R I A  C H A T  B O T |"
+                      + "\n\\-------------------------------/");
     }
 }
     
